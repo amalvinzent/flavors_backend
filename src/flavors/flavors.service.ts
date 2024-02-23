958768487a67c855079b008342364924dd1f84b8
amalvinzent <amalvincent24@gmail.com>
2024-02-23 19:03:37 +0530

    removing log

--- src/flavors/flavors.service.ts
+++ src/flavors/flavors.service.ts
@@ -1,58 +1,60 @@
-import { Injectable } from '@nestjs/common'
-import { log } from 'console'
-import { readFile } from 'src/utils/utils'
-import { QueryParamsDto, SortOrder } from './dto/flavor.dto'
+import { Injectable } from "@nestjs/common";
+import { log } from "console";
+import { readFile } from "src/utils/utils";
+import { QueryParamsDto, SortOrder } from "./dto/flavor.dto";
 
 export type Records = Array<{
-  key: string
-  name: string
-  ingredients: string
-  diet: string
-  prep_time: number
-  cook_time: number
-  flavor_profile: string
-  course: string
-  state: string
-  region: string
-}>
+  key: string;
+  name: string;
+  ingredients: string;
+  diet: string;
+  prep_time: number;
+  cook_time: number;
+  flavor_profile: string;
+  course: string;
+  state: string;
+  region: string;
+}>;
 
 @Injectable()
 export class FlavorsService {
   async findAll(query_params: QueryParamsDto) {
-    let result: Records = await readFile('./src/flavors/data/flavors.json')
+    let result: Records = await readFile("./src/flavors/data/flavors.json");
 
     result = result?.filter((ob) => {
       if (Object.values(ob).some((value) => Math.sign(Number(value)) === -1))
-        return false
+        return false;
       return Object.keys(query_params?.filter || {}).every((key) => {
-        return ob.hasOwnProperty(key) && ob[key] === query_params?.filter[key]
-      })
-    })
+        return ob.hasOwnProperty(key) && ob[key] === query_params?.filter[key];
+      });
+    });
 
     result = result?.filter((item) => {
-      return ['name', 'state', 'region'].some((key) => {
+      return ["name", "state", "region"].some((key) => {
         return String(item[key])
           ?.toLowerCase()
-          ?.includes(query_params?.search?.toLowerCase())
-      })
-    })
+          ?.includes(query_params?.search?.toLowerCase());
+      });
+    });
 
-    const sort_key = Object.keys(query_params?.sort)[0]
-    const sort_order = Object.values(query_params?.sort)[0]
+    const sort_key = Object.keys(query_params?.sort)[0];
+    const sort_order = Object.values(query_params?.sort)[0];
     result = result?.sort((a, b) => {
-      const asc_desc = sort_order === SortOrder.ASC ? 1 : -1
-      if (typeof a[sort_key] === 'number' && typeof b[sort_key] === 'number') {
-        return (a[sort_key] - b[sort_key]) * asc_desc
+      const asc_desc = sort_order === SortOrder.ASC ? 1 : -1;
+      if (typeof a[sort_key] === "number" && typeof b[sort_key] === "number") {
+        return (a[sort_key] - b[sort_key]) * asc_desc;
       } else {
-        return String(a[sort_key]).localeCompare(String(b[sort_key])) * asc_desc
+        return (
+          String(a[sort_key]).localeCompare(String(b[sort_key])) * asc_desc
+        );
       }
-    })
+    });
 
-    const total = result?.length
+    const total = result?.length;
     result = result?.slice(
       (Number(query_params?.page_number) - 1) * Number(query_params?.page_size),
       Number(query_params?.page_number) * Number(query_params?.page_size)
-    )
+    );
 
     return {
       result: result || [],
@@ -60,34 +62,33 @@ export class FlavorsService {
         total: total || 0,
         count: result?.length || 0,
         limit: Number(query_params?.page_size),
-        offset: Number(query_params?.page_number)
-      }
-    }
+        offset: Number(query_params?.page_number),
+      },
+    };
   }
 
   async findById(id: string) {
-    const result: Records = await readFile('./src/flavors/data/flavors.json')
-    return result?.filter((v) => v?.key == id)[0] || {}
+    const result: Records = await readFile("./src/flavors/data/flavors.json");
+    return result?.filter((v) => v?.key == id)[0] || {};
   }
 
   async suggestor(data) {
-    log(data)
     if (data?.ingredients) {
-      let result: Records = await readFile('./src/flavors/data/flavors.json')
-      let new_arr = []
+      let result: Records = await readFile("./src/flavors/data/flavors.json");
+      let new_arr = [];
       for (let i = 0; i < result?.length; i++) {
         const split = result[i]?.ingredients
-          ?.split(', ')
-          ?.map((item) => item?.trim())
+          ?.split(", ")
+          ?.map((item) => item?.trim());
 
         let diff = data?.ingredients?.every((ingredient) =>
           split?.includes(ingredient)
-        )
+        );
         if (diff) {
-          new_arr.push(result[i])
+          new_arr.push(result[i]);
         }
       }
-      return new_arr
+      return new_arr;
     }
   }
 }
