c996a921933520b92c6d8dc8eb042352ac0f43a2
amalvinzent <amalvincent24@gmail.com>
2024-02-26 10:50:29 +0530

    response compression

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { useContainer } from 'class-validator'
+import * as compression from 'compression'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bodyParser: true })
+  app.use(compression())
   app.enableCors()
   useContainer(app.select(AppModule), { fallbackOnErrors: true })
   await app.listen(3000)
