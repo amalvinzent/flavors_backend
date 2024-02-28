6e6bb6d18cb74db31ed87ad0a96330393b369008
amalvinzent <amalvincent24@gmail.com>
2024-02-28 19:08:06 +0530

    changing variable names for better readability

--- src/flavors/flavors.service.ts
+++ src/flavors/flavors.service.ts
@@ -19,17 +19,13 @@ export type Records = Array<{
 @Injectable()
 export class FlavorsService {
   async findAll(query_params: QueryParamsDto) {
-    let result: Records = await readFile('./src/flavors/data/flavors.json')
-    result = result?.filter((ob) => {
-      if (
-        Object.values(ob || [])?.some(
-          (value) => Math.sign(Number(value)) === -1
-        )
-      ) {
+    const result: Records = await readFile('./src/flavors/data/flavors.json')
+    const filtered_results = result?.filter((ob) => {
+      if (Object.values(ob || {}).some((value) => Number(value) < 0)) {
         return false
       }
-      if (Object.keys(query_params?.filter || [])?.length) {
-        const filter_match = Object.keys(query_params?.filter || []).every(
+      if (Object.keys(query_params?.filter || {})?.length) {
+        const filter_match = Object.keys(query_params?.filter || {})?.every(
           (key) =>
             ob?.hasOwnProperty(key) && ob[key] === query_params?.filter[key]
         )
@@ -38,45 +34,41 @@ export class FlavorsService {
         }
       }
       if (query_params?.search) {
+        const search_query = query_params?.search
+          ?.toLowerCase()
+          ?.replace(/\s+/g, '')
         return ['name', 'state', 'region'].some((key) => {
-          if (
-            !ob[key]
-              ?.toLowerCase()
-              ?.replace(/\s+/g, '')
-              ?.includes(
-                query_params?.search?.toLowerCase()?.replace(/\s+/g, '')
-              )
-          ) {
-            return false
-          }
-          return true
+          const field_value = ob[key]?.toLowerCase()?.replace(/\s+/g, '')
+          return field_value?.includes(search_query)
         })
       }
       return true
     })
 
-    const sort_key = Object.keys(query_params?.sort || [])[0]
-    const sort_order = Object.values(query_params?.sort || [])[0]
-    result = result?.sort((a, b) => {
+    const sort_key = Object.keys(query_params?.sort || {})[0]
+    const sort_order = Object.values(query_params?.sort || {})[0]
+    const sorted_results = filtered_results?.sort((a, b) => {
       const asc_desc = sort_order === SortOrder.ASC ? 1 : -1
       if (typeof a[sort_key] === 'number' && typeof b[sort_key] === 'number') {
         return (a[sort_key] - b[sort_key]) * asc_desc
       } else {
-        return String(a[sort_key]).localeCompare(String(b[sort_key])) * asc_desc
+        return (
+          String(a[sort_key])?.localeCompare(String(b[sort_key])) * asc_desc
+        )
       }
     })
 
-    const total = result?.length
-    result = result?.slice(
+    const total = sorted_results?.length
+    const paginated_results = sorted_results?.slice(
       (Number(query_params?.page_number) - 1) * Number(query_params?.page_size),
       Number(query_params?.page_number) * Number(query_params?.page_size)
     )
 
     return {
-      result: result || [],
+      result: paginated_results || [],
       pagination: {
         total: total || 0,
-        count: result?.length || 0,
+        count: paginated_results?.length || 0,
         limit: Number(query_params?.page_size),
         offset: Number(query_params?.page_number)
       }
